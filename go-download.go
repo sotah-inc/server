8342a29a6eb917d030c8da9ddbbb0d82dc230a54
Adrian Parker <iamhiddensomewhere@gmail.com>
2013-11-23 19:39:05 -0500

    - some refactoring to make the main function more readable

--- go-download.go
+++ go-download.go
@@ -11,127 +11,158 @@ import (
 	"os"
 )
 
-func initialize(args []string) (configFile Config.ConfigFile, client Cache.Client, err error) {
+func initialize(args []string) (Config.ConfigFile, Cache.Client, error) {
+	var (
+		configFile Config.ConfigFile
+		client     Cache.Client
+		err        error
+	)
+
 	if len(args) == 1 {
 		err = errors.New("Expected path to config file, got nothing")
-		return
+		return configFile, client, err
 	}
 
 	// loading the config-file
 	configFile, err = Config.New(args[1])
 	if err != nil {
-		return
+		return configFile, client, err
 	}
 
 	// connecting the redis clients
 	client, err = Config.NewCacheClient(configFile.ConnectionList)
 	if err != nil {
-		return
+		return configFile, client, err
 	}
 
 	// flushing all of the databases
 	err = client.FlushDb()
 	if err != nil {
-		return
+		return configFile, client, err
 	}
 
-	return
+	return configFile, client, nil
 }
 
-func load(configRegions []Config.Region) (regions []Entity.Region) {
-
-}
-
-func main() {
-	Util.Write("Starting...")
-
+func load(client Cache.Client, configRegions []Config.Region) ([]Entity.Region, error) {
 	var (
-		err        error
-		client     Cache.Client
-		configFile Config.ConfigFile
+		regions []Entity.Region
+		err     error
 	)
-
-	/*
-		initialization
-	*/
-	configFile, client, err = initialize(os.Args)
-	if err != nil {
-		Util.Write(fmt.Sprintf("initialize() fail: %s", err.Error()))
-		return
-	}
-
-	/*
-		reading the config
-	*/
-	// managers
 	regionManager := Entity.RegionManager{Client: client}
 	localeManager := Entity.LocaleManager{Client: client}
-	realmManager := Entity.RealmManager{Client: client}
 
-	// persisting the regions and locales
-	Util.Write(fmt.Sprintf("Persisting %d regions...", len(configFile.Regions)))
-	regions := map[int64]Entity.Region{}
-	for _, configRegion := range configFile.Regions {
+	for _, configRegion := range configRegions {
 		region := configRegion.ToEntity()
 		region, err = regionManager.Persist(region)
 		if err != nil {
-			Util.Write(fmt.Sprintf("regionManager.Persist() fail: %s", err.Error()))
-			return
+			return regions, err
 		}
 
-		Util.Write(fmt.Sprintf("Persisting %d locales belonging to %s...", len(configRegion.Locales), region.Name))
 		for _, configLocale := range configRegion.Locales {
 			locale := configLocale.ToEntity()
 			locale.Region = region
 			locale, err = localeManager.Persist(locale)
 			if err != nil {
-				Util.Write(fmt.Sprintf("localeManager.Persist() fail: ", err.Error()))
-				return
+				return regions, err
 			}
 		}
-		regions[region.Id] = region
+		regions = append(regions, region)
 	}
+	return regions, nil
+}
+
+func getRealms(client Cache.Client, regions []Entity.Region) (map[int64][]Entity.Realm, error) {
+	var (
+		regionRealms map[int64][]Entity.Realm
+		err          error
+	)
+	regionRealms = map[int64][]Entity.Realm{}
+	realmManager := Entity.RealmManager{Client: client}
 
 	// going over the regions to download the statuses
-	Util.Write(fmt.Sprintf("Going over %d regions to download the statuses...", len(regions)))
 	c := make(chan Status.Result, len(regions))
 	for _, region := range regions {
 		go Status.Get(region, c)
 	}
 
 	// gathering the results
-	Util.Write(fmt.Sprintf("Gathering the results for %d regions...", len(regions)))
 	results := make([]Status.Result, len(regions))
 	for i := 0; i < len(results); i++ {
 		results[i] = <-c
 	}
 
 	// going over the results
-	Util.Write(fmt.Sprintf("Going over %d results...", len(results)))
-	realmCount := 0
 	for _, result := range results {
 		if err = result.Error; err != nil {
-			Util.Write(fmt.Sprintf("Status.Get() fail: %s", err.Error()))
-			return
+			return regionRealms, err
 		}
 
 		region := result.Region
 		statusRealms := result.Status.Realms
-		Util.Write(fmt.Sprintf("Persisting %d realms belonging to %s...", len(statusRealms), region.Name))
 		for _, statusRealm := range statusRealms {
 			realm := statusRealm.ToEntity()
 			realm.Region = region
-
 			realm, err = realmManager.Persist(realm)
 			if err != nil {
-				Util.Write(fmt.Sprintf("realmManager.Persist() fail: %s", err.Error()))
-				return
+				return regionRealms, err
 			}
 
-			realmCount++
+			regionRealms[region.Id] = append(regionRealms[region.Id], realm)
 		}
 	}
-	Util.Write(fmt.Sprintf("Persisted %d realms!", realmCount))
+
+	return regionRealms, nil
+}
+
+func main() {
+	Util.Write("Starting...")
+
+	var (
+		err          error
+		client       Cache.Client
+		configFile   Config.ConfigFile
+		regions      []Entity.Region
+		regionRealms map[int64][]Entity.Realm
+	)
+
+	/*
+		reading the config
+	*/
+	// getting a client
+	Util.Write("Initializing the config...")
+	configFile, client, err = initialize(os.Args)
+	if err != nil {
+		Util.Write(fmt.Sprintf("initialize() fail: %s", err.Error()))
+		return
+	}
+
+	// loading the regions and locales
+	Util.Write("Loading the regions and locales...")
+	regions, err = load(client, configFile.Regions)
+	if err != nil {
+		Util.Write(fmt.Sprintf("load() fail: %s", err.Error()))
+		return
+	}
+
+	/*
+		gathering and persisting realms for each region
+	*/
+	Util.Write("Fetching realms for each region...")
+	regionRealms, err = getRealms(client, regions)
+	if err != nil {
+		Util.Write(fmt.Sprintf("getRealms() fail: %s", err.Error()))
+		return
+	}
+
+	/*
+		debug
+	*/
+	realmCount := 0
+	for _, realms := range regionRealms {
+		realmCount += len(realms)
+	}
+	Util.Write(fmt.Sprintf("Persisted %d realms...", realmCount))
 
 	Util.Conclude()
 }
