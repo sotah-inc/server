dc7c6f4e771181f47dbb66ac293f9365e1f883e6
Adrian Parker <iamhiddensomewhere@gmail.com>
2014-12-28 11:41:43 -0500

    - further refactoring for cleanliness

--- Work/ItemizeResult.go
+++ Work/ItemizeResult.go
@@ -12,31 +12,3 @@ type ItemizeResult struct {
 	Characters   []Entity.Character
 	Auctions     []AuctionData.Auction
 }
-
-/*
-	ItemizeResults
-*/
-type ItemizeResults struct {
-	List []ItemizeResult
-}
-
-func (self ItemizeResults) GetUniqueItems() (items []Entity.Item) {
-	blizzItemIds := make(map[int64]struct{})
-	for _, result := range self.List {
-		for _, blizzItemId := range result.BlizzItemIds {
-			_, valid := blizzItemIds[blizzItemId]
-			if !valid {
-				blizzItemIds[blizzItemId] = struct{}{}
-			}
-		}
-	}
-
-	items = make([]Entity.Item, len(blizzItemIds))
-	i := 0
-	for blizzItemId, _ := range blizzItemIds {
-		items[i] = Entity.Item{BlizzId: blizzItemId}
-		i++
-	}
-
-	return items
-}
---
+++ Work/ItemizeResults.go
@@ -0,0 +1,30 @@
+package Work
+
+import (
+	"github.com/ihsw/go-download/Entity"
+)
+
+type ItemizeResults struct {
+	List []ItemizeResult
+}
+
+func (self ItemizeResults) GetUniqueItems() (items []Entity.Item) {
+	blizzItemIds := make(map[int64]struct{})
+	for _, result := range self.List {
+		for _, blizzItemId := range result.BlizzItemIds {
+			_, valid := blizzItemIds[blizzItemId]
+			if !valid {
+				blizzItemIds[blizzItemId] = struct{}{}
+			}
+		}
+	}
+
+	items = make([]Entity.Item, len(blizzItemIds))
+	i := 0
+	for blizzItemId, _ := range blizzItemIds {
+		items[i] = Entity.Item{BlizzId: blizzItemId}
+		i++
+	}
+
+	return items
+}
