6a3510c6ec3d47c1a417314f718e396ab285ed9e
Adrian Parker <iamhiddensomewhere@gmail.com>
2015-02-15 01:12:51 -0500

    - removing unused

--- Work/ItemizeResult.go
+++ Work/ItemizeResult.go
@@ -1,10 +1,5 @@
 package Work
 
-import (
-	"github.com/ihsw/go-download/Blizzard/AuctionData"
-	"github.com/ihsw/go-download/Entity/Character"
-)
-
 type ItemizeResult struct {
 	Result
 	blizzItemIds []int64
