3e025b31723b5fc8331c432c9d262f763a4d82a2
Adrian Parker <adrian@leapshot.com>
2015-02-25 11:12:33 -0500

    - continuing the pipeline examples

--- .unbuffered-example.go
+++ .unbuffered-example.go
@@ -14,16 +14,35 @@ type middleJobHandler struct {
 }
 
 func newMiddleJobHandler(workerCount int, out chan job) middleJobHandler {
-	jobHandler := middleJobHandler{
+	return middleJobHandler{
 		jobHandler: newJobHandler(workerCount, out),
 	}
-	return jobHandler
 }
 
 func (self middleJobHandler) process(job job) job {
 	fmt.Println(fmt.Sprintf("middle working on %s", job.url))
 	time.Sleep(time.Second * 5)
 	job.middleFinishTime = time.Now()
+	return job
+}
+
+/*
+	inJobHandler
+*/
+type inJobHandler struct {
+	jobHandler
+}
+
+func newInJobHandler(workerCount int, out chan job) inJobHandler {
+	return inJobHandler{
+		jobHandler: newJobHandler(workerCount, out),
+	}
+}
+
+func (self inJobHandler) process(job job) job {
+	fmt.Println(fmt.Sprintf("in working on %s", job.url))
+	time.Sleep(time.Second * 2)
+	job.inFinishTime = time.Now()
 	return job
 }
 
@@ -63,9 +82,6 @@ func newJobHandler(workerCount int, out chan job) jobHandler {
 }
 
 func (self jobHandler) process(job job) job {
-	fmt.Println(fmt.Sprintf("working on %s", job.url))
-	time.Sleep(time.Second * 2)
-	job.inFinishTime = time.Now()
 	return job
 }
 
@@ -86,8 +102,8 @@ type job struct {
 func main() {
 	out := make(chan job)
 	outJobHandler := newJobHandler(1, out)
-	middleJobHandler := newJobHandler(1, outJobHandler.in)
-	inJobHandler := newJobHandler(4, middleJobHandler.in)
+	middleJobHandler := newMiddleJobHandler(1, outJobHandler.in)
+	inJobHandler := newInJobHandler(4, middleJobHandler.in)
 
 	// queueing up the in channel
 	urls := []string{"http://google.ca/", "http://golang.org/", "http://youtube.com/"}
@@ -106,8 +122,8 @@ func main() {
 	const WriteLayout = "2006-01-02 03:04:05PM"
 	for job := range out {
 		fmt.Println(fmt.Sprintf("job %s finished: %v", job.url, job.done))
-		fmt.Println(fmt.Sprintf("job %s start time: %s", job.url, job.startTime.Format(WriteLayout)))
-		fmt.Println(fmt.Sprintf("job %s in finish time: %s", job.url, job.inFinishTime.Format(WriteLayout)))
-		fmt.Println(fmt.Sprintf("job %s middle finish time: %s", job.url, job.middleFinishTime.Format(WriteLayout)))
+		fmt.Println(fmt.Sprintf("%s is the start time", job.startTime.Format(WriteLayout)))
+		fmt.Println(fmt.Sprintf("%s is the in finish time", job.inFinishTime.Format(WriteLayout)))
+		fmt.Println(fmt.Sprintf("%s is the middle finish time", job.middleFinishTime.Format(WriteLayout)))
 	}
 }
