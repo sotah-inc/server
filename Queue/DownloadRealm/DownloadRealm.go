13f1b34d6838a7608691b82f81b57b37ddddc551
Adrian Parker <iamhiddensomewhere@gmail.com>
2015-03-02 17:48:36 -0500

    - continuing migration to the new queue system

--- Queue/DownloadRealm/DownloadRealm.go
+++ Queue/DownloadRealm/DownloadRealm.go
@@ -8,6 +8,7 @@ import (
 	"github.com/ihsw/go-download/Blizzard/AuctionData"
 	"github.com/ihsw/go-download/Cache"
 	"github.com/ihsw/go-download/Entity"
+	"github.com/ihsw/go-download/Entity/Character"
 	"github.com/ihsw/go-download/Queue"
 	"github.com/ihsw/go-download/Util"
 	"io/ioutil"
@@ -95,6 +96,41 @@ func newJob(realm Entity.Realm) Job {
 type Job struct {
 	Queue.AuctionDataJob
 	AuctionDataResponse *AuctionData.Response
+}
+
+func (self Job) GetNewCharacters(existingNames []string) (newCharacters []Character.Character) {
+	// misc
+	auctions := self.AuctionDataResponse.Auctions.Auctions
+
+	// gathering the names for uniqueness
+	existingNameFlags := make(map[string]struct{})
+	for _, name := range existingNames {
+		existingNameFlags[name] = struct{}{}
+	}
+
+	newNames := make(map[string]struct{})
+	for _, auction := range auctions {
+		name := auction.Owner
+		_, ok := existingNameFlags[name]
+		if ok {
+			continue
+		}
+
+		newNames[name] = struct{}{}
+	}
+
+	// doing a second pass to fill new ones in
+	newCharacters = make([]Character.Character, len(newNames))
+	i := 0
+	for name, _ := range newNames {
+		newCharacters[i] = Character.Character{
+			Name:  name,
+			Realm: self.Realm,
+		}
+		i++
+	}
+
+	return newCharacters
 }
 
 func (self Job) dumpData() (err error) {
--- Queue/ItemizeRealm/ItemizeRealm.go
+++ Queue/ItemizeRealm/ItemizeRealm.go
@@ -1,7 +1,10 @@
 package ItemizeRealm
 
 import (
+	"errors"
+	"fmt"
 	"github.com/ihsw/go-download/Cache"
+	"github.com/ihsw/go-download/Entity/Character"
 	"github.com/ihsw/go-download/Queue"
 	"github.com/ihsw/go-download/Queue/DownloadRealm"
 )
@@ -26,8 +29,35 @@ func DoWork(in chan DownloadRealm.Job, cacheClient Cache.Client) chan Job {
 func process(inJob DownloadRealm.Job, cacheClient Cache.Client) (job Job) {
 	// misc
 	job = newJob(inJob)
+	realm := inJob.Realm
+
+	if inJob.Err != nil {
+		job.Err = errors.New(fmt.Sprintf("DownloadRealm.Job() had an error (%s)", inJob.Err.Error()))
+		return
+	}
 
 	if !inJob.CanContinue() {
+		return
+	}
+
+	/*
+		character handling
+	*/
+	characterManager := Character.NewManager(realm, cacheClient)
+
+	// gathering existing characters
+	var (
+		existingNames []string
+		err           error
+	)
+	if existingNames, err = characterManager.GetNames(); err != nil {
+		job.Err = errors.New(fmt.Sprintf("CharacterManager.GetNames() failed (%s)", err.Error()))
+		return
+	}
+
+	// gathering new characters
+	if err = characterManager.PersistAll(inJob.GetNewCharacters(existingNames)); err != nil {
+		job.Err = errors.New(fmt.Sprintf("CharacterManager.PersistAll() failed (%s)", err.Error()))
 		return
 	}
 
