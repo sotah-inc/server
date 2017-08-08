c251b9c35ac368bde0517ceccc009acf59702b39
Adrian Parker <ihsw.aparker@gmail.com>
2017-08-08 08:37:07 -0400

    Add Work() func for parallel execution.

--- app/util/util.go
+++ app/util/util.go
@@ -5,7 +5,25 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"sync"
 )
+
+// Work - spawns N number of goroutines to execute X() in parallel, with Y() called when they exit
+func Work(workerCount int, worker func(), postWork func()) {
+	wg := &sync.WaitGroup{}
+	wg.Add(workerCount)
+	for i := 0; i < workerCount; i++ {
+		go func() {
+			defer wg.Done()
+			worker()
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		postWork()
+	}()
+}
 
 // Download - performs HTTP GET request against url, including adding gzip header and ungzipping
 func Download(url string) (b []byte, err error) {
