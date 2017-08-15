2d1eff3473743d2677c7f34cb1220f6ccf0b84a8
Adrian Parker <ihsw.aparker@gmail.com>
2017-08-15 14:05:34 -0400

    Add newResolver() to return default resolver funcs.

--- app/resolver.go
+++ app/resolver.go
@@ -1,5 +1,13 @@
 package main
 
+func newResolver() resolver {
+	return resolver{
+		getStatusURL:      defaultGetStatusURL,
+		getAuctionInfoURL: defaultGetAuctionInfoURL,
+		getAuctionsURL:    defaultGetAuctionsURL,
+	}
+}
+
 type resolver struct {
 	getStatusURL      getStatusURLFunc
 	getAuctionInfoURL getAuctionInfoURLFunc
