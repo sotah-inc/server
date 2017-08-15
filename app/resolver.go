05d7470a94b9c8b79b6c81408f477d04c79e971e
Adrian Parker <ihsw.aparker@gmail.com>
2017-08-15 14:31:22 -0400

    Add appendAPIKey() func.

--- app/resolver.go
+++ app/resolver.go
@@ -1,5 +1,7 @@
 package main
 
+import "net/url"
+
 func newResolver() resolver {
 	return resolver{
 		getStatusURL:      defaultGetStatusURL,
@@ -8,7 +10,22 @@ func newResolver() resolver {
 	}
 }
 
+func (r resolver) appendAPIKey(urlValue string) (string, error) {
+	u, err := url.Parse(urlValue)
+	if err != nil {
+		return "", err
+	}
+
+	q := u.Query()
+	q.Set("apikey", r.apiKey)
+	u.RawQuery = q.Encode()
+
+	return u.String(), nil
+}
+
 type resolver struct {
+	apiKey string
+
 	getStatusURL      getStatusURLFunc
 	getAuctionInfoURL getAuctionInfoURLFunc
 	getAuctionsURL    getAuctionsURLFunc
