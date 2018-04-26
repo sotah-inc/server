e58e86cfbc3a544357682d769605c20cb7b74afe
Adrian Parker <iamhiddensomewhere@gmail.com>
2018-04-26 06:46:11 -0400

    Add item-reversed auctions sorter.

--- app/miniauctions_sorters.go
+++ app/miniauctions_sorters.go
@@ -4,19 +4,29 @@ import "sort"
 
 type miniAuctionSortFn func(miniAuctionList) miniAuctionList
 
-type miniAuctionSorter map[string]miniAuctionSortFn
-
-func getMiniAuctionSorter() miniAuctionSorter {
+func newMiniAuctionSorter() miniAuctionSorter {
 	return miniAuctionSorter{
 		"item": func(mAuctionList miniAuctionList) miniAuctionList {
 			sort.Sort(byItem(mAuctionList))
 			return mAuctionList
 		},
+		"item-r": func(mAuctionList miniAuctionList) miniAuctionList {
+			sort.Sort(byItemReversed(mAuctionList))
+			return mAuctionList
+		},
 	}
 }
+
+type miniAuctionSorter map[string]miniAuctionSortFn
 
 type byItem miniAuctionList
 
 func (bi byItem) Len() int           { return len(bi) }
 func (bi byItem) Swap(i, j int)      { bi[i], bi[j] = bi[j], bi[i] }
 func (bi byItem) Less(i, j int) bool { return bi[i].Item < bi[j].Item }
+
+type byItemReversed miniAuctionList
+
+func (bi byItemReversed) Len() int           { return len(bi) }
+func (bi byItemReversed) Swap(i, j int)      { bi[i], bi[j] = bi[j], bi[i] }
+func (bi byItemReversed) Less(i, j int) bool { return bi[i].Item > bi[j].Item }
--- app/miniauctions_sorters_test.go
+++ app/miniauctions_sorters_test.go
@@ -12,9 +12,27 @@ func TestMiniAuctionsSortByItem(t *testing.T) {
 		return
 	}
 
-	sorter := getMiniAuctionSorter()
+	sorter := newMiniAuctionSorter()
 	sortedMad := sorter["item"](mad.Auctions)
 	expectedSortedMad, err := newMiniAuctionsDataFromFilepath("./TestData/mini-auctions-sort-by-item.json")
+	if !assert.Nil(t, err) {
+		return
+	}
+
+	if !assert.Equal(t, expectedSortedMad.Auctions, sortedMad) {
+		return
+	}
+}
+
+func TestMiniAuctionsSortByItemReversed(t *testing.T) {
+	mad, err := newMiniAuctionsDataFromFilepath("./TestData/mini-auctions.json")
+	if !assert.Nil(t, err) {
+		return
+	}
+
+	sorter := newMiniAuctionSorter()
+	sortedMad := sorter["item-r"](mad.Auctions)
+	expectedSortedMad, err := newMiniAuctionsDataFromFilepath("./TestData/mini-auctions-sort-by-item-reversed.json")
 	if !assert.Nil(t, err) {
 		return
 	}
