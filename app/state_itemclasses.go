5d7d80649943b11aa7ffddd1211a238a82461c65
Adrian Parker <aparker@industrialagency.ca>
2018-06-27 13:28:16 -0400

    Remove unnecessary file with single func.

--- app/state_itemclasses.go
+++ app/state_itemclasses.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 
+	"github.com/ihsw/sotah-server/app/blizzard"
 	"github.com/ihsw/sotah-server/app/codes"
 	"github.com/ihsw/sotah-server/app/subjects"
 	nats "github.com/nats-io/go-nats"
 )
+
+func newItemClassesFromMessenger(mess messenger) (blizzard.ItemClasses, error) {
+	msg, err := mess.request(subjects.ItemClasses, []byte{})
+	if err != nil {
+		return blizzard.ItemClasses{}, err
+	}
+
+	if msg.Code != codes.Ok {
+		return blizzard.ItemClasses{}, errors.New(msg.Err)
+	}
+
+	return blizzard.NewItemClasses([]byte(msg.Data))
+}
 
 func (sta state) listenForItemClasses(stop chan interface{}) error {
 	err := sta.messenger.subscribe(subjects.ItemClasses, stop, func(natsMsg nats.Msg) {
