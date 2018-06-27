cdeb2edd14e499a10d6d5dc1fafcc40dfe157c89
Adrian Parker <aparker@industrialagency.ca>
2018-06-27 08:26:57 -0400

    Move status-related functionality to the realms file.

--- app/realms.go
+++ app/realms.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/ihsw/sotah-server/app/blizzard"
+	"github.com/ihsw/sotah-server/app/codes"
+	"github.com/ihsw/sotah-server/app/subjects"
 	"github.com/ihsw/sotah-server/app/util"
 	log "github.com/sirupsen/logrus"
 )
@@ -152,4 +155,47 @@ func (rea realm) getAuctions(res resolver) (auctions, error) {
 
 	rea.LogEntry().Debug("Loading auction data from cache dir")
 	return newAuctionsFromGzFilepath(rea, auctionsFilepath)
+}
+
+func newStatusFromMessenger(reg region, mess messenger) (status, error) {
+	lm := statusRequest{RegionName: reg.Name}
+	encodedMessage, err := json.Marshal(lm)
+	if err != nil {
+		return status{}, err
+	}
+
+	msg, err := mess.request(subjects.Status, encodedMessage)
+	if err != nil {
+		return status{}, err
+	}
+
+	if msg.Code != codes.Ok {
+		return status{}, errors.New(msg.Err)
+	}
+
+	stat, err := blizzard.NewStatus([]byte(msg.Data))
+	if err != nil {
+		return status{}, err
+	}
+
+	return newStatus(reg, stat), nil
+}
+
+func newStatusFromFilepath(reg region, relativeFilepath string) (status, error) {
+	stat, err := blizzard.NewStatusFromFilepath(relativeFilepath)
+	if err != nil {
+		return status{}, err
+	}
+
+	return newStatus(reg, stat), nil
+}
+
+func newStatus(reg region, stat blizzard.Status) status {
+	return status{stat, reg, newRealms(reg, stat.Realms)}
+}
+
+type status struct {
+	blizzard.Status
+	region region
+	Realms realms
 }
