2b2c70054d77f7904cf460d2b24498a20fdb1c2c
Adrian Parker <ihsw.aparker@gmail.com>
2018-08-11 21:24:20 -0400

    Move api-test code to its own file.

--- app/commands.go
+++ app/commands.go
@@ -4,127 +4,12 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
-	"path/filepath"
 
 	"github.com/ihsw/sotah-server/app/blizzard"
 	"github.com/ihsw/sotah-server/app/subjects"
 	"github.com/ihsw/sotah-server/app/util"
 	log "github.com/sirupsen/logrus"
 )
-
-func apiTest(c config, m messenger, dataDir string) error {
-	log.Info("Starting api-test")
-
-	dataDirPath, err := filepath.Abs(dataDir)
-	if err != nil {
-		return err
-	}
-
-	// preloading the status file and auctions file
-	statusBody, err := util.ReadFile(fmt.Sprintf("%s/realm-status.json", dataDirPath))
-	if err != nil {
-		return err
-	}
-	auc, err := blizzard.NewAuctionsFromFilepath(fmt.Sprintf("%s/auctions.json", dataDirPath))
-	if err != nil {
-		return err
-	}
-
-	// establishing a state and filling it with statuses
-	res := newResolver(c, m)
-	sta := state{
-		messenger: m,
-		resolver:  res,
-		regions:   c.Regions,
-		statuses:  map[regionName]status{},
-		auctions:  map[regionName]map[blizzard.RealmSlug]miniAuctionList{},
-		items:     map[blizzard.ItemID]blizzard.Item{},
-	}
-	for _, reg := range c.Regions {
-		// loading realm statuses
-		stat, err := blizzard.NewStatus(statusBody)
-		if err != nil {
-			return err
-		}
-		sta.statuses[reg.Name] = status{Status: stat, region: reg}
-
-		// misc
-		regionItemIDsMap := map[blizzard.ItemID]struct{}{}
-
-		// loading realm auctions
-		sta.auctions[reg.Name] = map[blizzard.RealmSlug]miniAuctionList{}
-		for _, rea := range stat.Realms {
-			maList := newMiniAuctionListFromBlizzardAuctions(auc.Auctions)
-
-			sta.auctions[reg.Name][rea.Slug] = maList
-
-			for _, ID := range maList.itemIds() {
-				_, ok := sta.items[ID]
-				if ok {
-					continue
-				}
-
-				regionItemIDsMap[ID] = struct{}{}
-			}
-		}
-
-		// gathering the list of item IDs for this region
-		regionItemIDs := make([]blizzard.ItemID, len(regionItemIDsMap))
-		i := 0
-		for ID := range regionItemIDsMap {
-			regionItemIDs[i] = ID
-			i++
-		}
-
-		// downloading items found in this region
-		log.WithField("items", len(regionItemIDs)).Info("Fetching items")
-		itemsOut := getItems(regionItemIDs, res)
-		for job := range itemsOut {
-			if job.err != nil {
-				log.WithFields(log.Fields{
-					"region": reg.Name,
-					"item":   job.ID,
-					"error":  job.err.Error(),
-				}).Info("Failed to fetch item")
-
-				continue
-			}
-
-			sta.items[job.ID] = job.item
-		}
-		log.WithField("items", len(regionItemIDs)).Info("Fetched items")
-	}
-
-	// opening all listeners
-	sta.listeners = newListeners(subjectListeners{
-		subjects.GenericTestErrors: sta.listenForGenericTestErrors,
-		subjects.Status:            sta.listenForStatus,
-		subjects.Regions:           sta.listenForRegions,
-		subjects.Auctions:          sta.listenForAuctions,
-		subjects.Owners:            sta.listenForOwners,
-		subjects.ItemsQuery:        sta.listenForItemsQuery,
-		subjects.AuctionsQuery:     sta.listenForAuctionsQuery,
-		subjects.ItemClasses:       sta.listenForItemClasses,
-		subjects.PriceList:         sta.listenForPriceList,
-		subjects.Items:             sta.listenForItems,
-	})
-	if err := sta.listeners.listen(); err != nil {
-		return err
-	}
-
-	// catching SIGINT
-	log.Info("Waiting for SIGINT")
-	sigIn := make(chan os.Signal, 1)
-	signal.Notify(sigIn, os.Interrupt)
-	<-sigIn
-
-	log.Info("Caught SIGINT, exiting")
-
-	// stopping listeners
-	sta.listeners.stop()
-
-	return nil
-}
 
 func api(c config, m messenger) error {
 	log.Info("Starting api")
