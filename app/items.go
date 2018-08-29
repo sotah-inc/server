1b4850b728658fddc77285ff69e3bc3bea61d897
Adrian Parker <ihsw.aparker@gmail.com>
2018-08-29 08:20:58 -0400

    Update item-fetching functionality to simultaneously work with both gcloud storage and disk.

--- app/items.go
+++ app/items.go
@@ -132,11 +132,6 @@ func getItem(ID blizzard.ItemID, res resolver) (blizzard.Item, error) {
 		return blizzard.Item{}, errors.New("Config cannot be nil")
 	}
 
-	primaryRegion, err := res.config.Regions.getPrimaryRegion()
-	if err != nil {
-		return blizzard.Item{}, err
-	}
-
 	if res.config.CacheDir == "" {
 		return blizzard.Item{}, errors.New("Cache dir cannot be blank")
 	}
@@ -151,13 +146,33 @@ func getItem(ID blizzard.ItemID, res resolver) (blizzard.Item, error) {
 			return blizzard.Item{}, err
 		}
 
+		// optionally checking gcloud store
+		if res.config.UseGCloudStorage {
+			exists, err := res.store.itemExists(ID)
+			if err != nil {
+				return blizzard.Item{}, err
+			}
+			if exists {
+				itemValue, _, err := res.store.loadItem(ID, res)
+				if err != nil {
+					return blizzard.Item{}, err
+				}
+
+				return itemValue, nil
+			}
+		}
+
 		log.WithField("item", ID).Info("Fetching item")
 
+		// checking blizzard api
+		primaryRegion, err := res.config.Regions.getPrimaryRegion()
+		if err != nil {
+			return blizzard.Item{}, err
+		}
 		uri, err := res.appendAPIKey(res.getItemURL(primaryRegion.Hostname, ID))
 		if err != nil {
 			return blizzard.Item{}, err
 		}
-
 		item, resp, err := blizzard.NewItemFromHTTP(uri)
 		if err != nil {
 			return blizzard.Item{}, err
@@ -166,8 +181,21 @@ func getItem(ID blizzard.ItemID, res resolver) (blizzard.Item, error) {
 			return blizzard.Item{}, err
 		}
 
+		// writing it back to disk
 		if err := util.WriteFile(itemFilepath, resp.Body); err != nil {
 			return blizzard.Item{}, err
+		}
+
+		// optionally writing it back to gcloud store
+		if res.config.UseGCloudStorage {
+			encodedBody, err := util.GzipEncode(resp.Body)
+			if err != nil {
+				return blizzard.Item{}, err
+			}
+
+			if err := res.store.writeItem(ID, encodedBody); err != nil {
+				return blizzard.Item{}, err
+			}
 		}
 
 		return item, nil
