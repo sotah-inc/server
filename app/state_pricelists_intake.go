4283ecf26e9ff49de9bb7750c2d1c0e98ad79ece
Adrian Parker <ihsw.aparker@gmail.com>
2018-09-11 13:09:42 -0400

    Add setting of realm-auctions object after pricelist intake is complete.

--- app/state_pricelists_intake.go
+++ app/state_pricelists_intake.go
@@ -3,6 +3,7 @@ package main
 import (
 	"time"
 
+	"cloud.google.com/go/storage"
 	"github.com/ihsw/sotah-server/app/logging"
 	"github.com/ihsw/sotah-server/app/subjects"
 	"github.com/ihsw/sotah-server/app/util"
@@ -38,6 +39,44 @@ func (sta state) listenForPricelistsIntake(stop listenStopChan) error {
 				}).Error("Failed to persist auctions to database")
 
 				continue
+			}
+
+			// optionally setting the obj state metadata to processed
+			if sta.resolver.config.UseGCloudStorage {
+				sto := sta.resolver.store
+
+				bkt := sto.getRealmAuctionsBucket(job.realm)
+				obj := bkt.Object(sto.getRealmAuctionsObjectName(job.lastModified))
+				objAttrs, err := obj.Attrs(sto.context)
+				if err != nil {
+					logging.WithFields(logrus.Fields{
+						"error":         err.Error(),
+						"region":        job.realm.region.Name,
+						"realm":         job.realm.Slug,
+						"last-modified": job.lastModified.Unix(),
+					}).Error("Failed to fetch obj attrs")
+
+					continue
+				}
+
+				objMeta := func() map[string]string {
+					if objAttrs.Metadata == nil {
+						return map[string]string{}
+					}
+
+					return objAttrs.Metadata
+				}()
+				objMeta["state"] = "processed"
+				if _, err := obj.Update(sto.context, storage.ObjectAttrsToUpdate{Metadata: objMeta}); err != nil {
+					logging.WithFields(logrus.Fields{
+						"error":         err.Error(),
+						"region":        job.realm.region.Name,
+						"realm":         job.realm.Slug,
+						"last-modified": job.lastModified.Unix(),
+					}).Error("Failed to update metadata of object")
+
+					continue
+				}
 			}
 		}
 	}
