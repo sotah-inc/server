1d0b4b5bea8e840610e65c57e34c7c713d684fa9
Adrian Parker <ihsw.aparker@gmail.com>
2018-09-15 17:06:23 -0400

    Update objstate suffix.

--- app/objstate/objstate.go
+++ app/objstate/objstate.go
@@ -7,6 +7,6 @@ type ObjState string
 ObjState - types of gcloud storage state
 */
 const (
-	Queued    ObjState = "queued-2018-09-15-1152"
-	Processed ObjState = "processed-2018-09-15-1152"
+	Queued    ObjState = "queued-2018-09-15-1706"
+	Processed ObjState = "processed-2018-09-15-1706"
 )
