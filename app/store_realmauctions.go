547b1c6454a00f586d81dacabf904a58f46dbb6a
Adrian Parker <ihsw.aparker@gmail.com>
2018-09-15 17:55:22 -0400

    Increasing worker count to 3.

--- app/store_realmauctions.go
+++ app/store_realmauctions.go
@@ -345,7 +345,7 @@ func (sto store) loadRegionRealmMap(rMap realmMap) chan loadAuctionsJob {
 	postWork := func() {
 		close(out)
 	}
-	util.Work(2, worker, postWork)
+	util.Work(3, worker, postWork)
 
 	// queueing up the realms
 	go func() {
