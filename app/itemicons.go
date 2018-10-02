1373462fb3f4bc92ccd573921457bfe1a915571a
Adrian Parker <ihsw.aparker@gmail.com>
2018-10-02 15:19:09 -0400

    Add character-icon URL format.

--- app/itemicons.go
+++ app/itemicons.go
@@ -5,6 +5,7 @@ import (
 )
 
 const itemIconURLFormat = "https://render-us.worldofwarcraft.com/icons/56/%s.jpg"
+const characterIconURLFormat = "https://render-us.worldofwarcraft.com/character/%s"
 
 func defaultGetItemIconURL(name string) string {
 	return fmt.Sprintf(itemIconURLFormat, name)
