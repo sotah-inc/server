b259ccbddac7f1f19cb1ba6def46e44b56236df9
Adrian Parker <ihsw.aparker@gmail.com>
2019-02-07 09:03:47 -0500

    Removing unused.

--- app/cmd/app/commands/commands.go
+++ app/cmd/app/commands/commands.go
@@ -6,10 +6,7 @@ type command string
 Commands - commands that run on main
 */
 var (
-	APITest            command = "api-test"
 	API                command = "api"
-	SyncItems          command = "sync-items"
 	LiveAuctions       command = "live-auctions"
 	PricelistHistories command = "pricelist-histories"
-	PruneStore         command = "prune-store"
 )
