a710dfdb11fad592f2e66e795fb7693a28e802d2
Adrian Parker <ihsw.aparker@gmail.com>
2019-02-15 20:09:24 -0500

    Fix func signature.

--- app/fn/test2/main.go
+++ app/fn/test2/main.go
@@ -2,6 +2,7 @@ package test2
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -22,10 +23,19 @@ func init() {
 	}
 }
 
-func HelloPubSub(ctx context.Context, m bus.Message) error {
+type PubSubMessage struct {
+	Data []byte `json:"data"`
+}
+
+func HelloPubSub(_ context.Context, m PubSubMessage) error {
+	var in bus.Message
+	if err := json.Unmarshal(m.Data, &in); err != nil {
+		return err
+	}
+
 	response := bus.NewMessage()
 	response.Data = fmt.Sprintf("Hello, %s!", m.Data)
-	_, err := busClient.ReplyTo(m, response)
+	_, err := busClient.ReplyTo(in, response)
 	if err != nil {
 		return err
 	}
