c01f7ef722b6d0e6125119ddddfadcc39f591838
Adrian Parker <ihsw.aparker@gmail.com>
2019-02-17 10:29:57 -0500

    Update to new topic.

--- app/bin/deploy-test2-func.sh
+++ app/bin/deploy-test2-func.sh
@@ -6,6 +6,6 @@ FN_DIR=`pwd`/fn/test2
 # deploying func
 gcloud functions deploy HelloPubSub \
     --runtime go111 \
-    --trigger-topic boot \
+    --trigger-topic auctionCount \
     --source $FN_DIR \
     --region us-central1 # because us-east1 does not fucking have go111 runtime yet
