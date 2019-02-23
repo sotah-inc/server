2ac0e58b3ca88c83a90ecb85076581d05513debf
Adrian Parker <ihsw.aparker@gmail.com>
2019-02-23 10:06:13 -0500

    Fill in testing of new pricelisthistory-based handle functionality.

--- app/pkg/command/command_pub.go
+++ app/pkg/command/command_pub.go
@@ -1,12 +1,18 @@
 package command
 
 import (
+	"errors"
 	"os"
 	"os/signal"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/sotah-inc/server/app/pkg/logging"
 	"github.com/sotah-inc/server/app/pkg/sotah"
 	"github.com/sotah-inc/server/app/pkg/state"
+	"github.com/sotah-inc/server/app/pkg/store"
+	"google.golang.org/api/iterator"
 )
 
 func Pub(config state.PubStateConfig) error {
@@ -32,6 +38,66 @@ func Pub(config state.PubStateConfig) error {
 	// opening all bus-listeners
 	logging.Info("Opening all bus-listeners")
 	pubState.BusListeners.Listen()
+
+	// gathering a realm
+	realm, err := func() (sotah.Realm, error) {
+		for regionName, status := range pubState.Statuses {
+			if regionName != "us" {
+				continue
+			}
+
+			for _, realm := range status.Realms {
+				if realm.Slug != "earthen-ring" {
+					continue
+				}
+
+				return realm, nil
+			}
+		}
+
+		return sotah.Realm{}, errors.New("could not find realm")
+	}()
+	if err != nil {
+		return err
+	}
+
+	phBase := store.NewPricelistHistoriesBase(pubState.IO.StoreClient)
+	bkt := pubState.IO.StoreClient.GetRealmAuctionsBucket(realm)
+	it := bkt.Objects(pubState.IO.StoreClient.Context, nil)
+	i := 0
+	for {
+		objAttrs, err := it.Next()
+		if err != nil {
+			if err == iterator.Done {
+				break
+			}
+
+			return err
+		}
+
+		obj := bkt.Object(objAttrs.Name)
+
+		s := strings.Split(objAttrs.Name, ".")
+		targetTimestamp, err := strconv.Atoi(s[0])
+		if err != nil {
+			return err
+		}
+		targetTime := time.Unix(int64(targetTimestamp), 0)
+
+		aucs, err := pubState.IO.StoreClient.NewAuctions(obj)
+		if err != nil {
+			return err
+		}
+
+		if _, err := phBase.Handle(aucs, targetTime, realm); err != nil {
+			return err
+		}
+
+		i++
+		if i > 5 {
+			break
+		}
+	}
 
 	// catching SIGINT
 	logging.Info("Waiting for SIGINT")
