ac896f223cccd8812529565415f8511680d68233
Adrian Parker <ihsw.aparker@gmail.com>
2019-02-24 21:09:41 -0500

    Fill in live-auctions-intake-v2 functionality.

--- app/pkg/metric/kinds/kinds.go
+++ app/pkg/metric/kinds/kinds.go
@@ -5,6 +5,7 @@ type Kind string
 const (
 	Collector                       Kind = "collector"
 	LiveAuctionsIntake              Kind = "liveauctions_intake"
+	LiveAuctionsIntakeV2            Kind = "liveauctions_intake_v2"
 	PricelistHistoriesIntake        Kind = "pricelisthistories_intake"
 	PricelistHistoriesIntakeV2      Kind = "pricelisthistories_intake_v2"
 	PricelistHistoriesComputeIntake Kind = "pricelisthistories_compute_intake"
