fad221b1dcbce240a779be1106f83a7a15998fe2
Adrian Parker <ihsw.aparker@gmail.com>
2019-02-25 19:57:31 -0500

    Fix typoes.

--- app/bin/deploy-live-auctions-intake-compute.sh
+++ app/bin/deploy-live-auctions-intake-compute.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 # gathering func dir
-FN_DIR=`pwd`/fn/live-auctions-intake-compute
+FN_DIR=`pwd`/fn/live-auctions-compute-intake
 
 # us-central1 because us-east1 does not fucking have go111 runtime yet
 
 # deploying func
-gcloud functions deploy LiveAuctionsIntakeCompute \
+gcloud functions deploy LiveAuctionsComputeIntake \
     --runtime go111 \
-    --trigger-topic pricelistHistoriesCompute \
+    --trigger-topic liveAuctionsCompute \
     --source $FN_DIR \
     --region us-central1
