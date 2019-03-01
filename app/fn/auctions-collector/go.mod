dcf55d6c8f855435e4077934237ce213885f4884
Adrian Parker <ihsw.aparker@gmail.com>
2019-03-01 10:35:59 -0500

    Update auctions-collector to clear all auction-manifest objects.

--- app/fn/auctions-collector/go.mod
+++ app/fn/auctions-collector/go.mod
@@ -2,5 +2,7 @@ module github.com/sotah-inc/server/app/fn/auctions-collector
 
 require (
 	cloud.google.com/go v0.35.1
-	github.com/sotah-inc/server/app v0.0.0-20190228193618-46f24e687200
+	github.com/sirupsen/logrus v1.3.0
+	github.com/sotah-inc/server/app v0.0.0-20190301021725-96941c11acec
+	google.golang.org/api v0.1.0
 )
--- app/fn/auctions-collector/main.go
+++ app/fn/auctions-collector/main.go
@@ -7,22 +7,27 @@ import (
 	"os"
 	"time"
 
-	"github.com/sirupsen/logrus"
-	"github.com/sotah-inc/server/app/pkg/logging"
-
 	"cloud.google.com/go/pubsub"
+	"github.com/sirupsen/logrus"
 	"github.com/sotah-inc/server/app/pkg/blizzard"
 	"github.com/sotah-inc/server/app/pkg/bus"
+	"github.com/sotah-inc/server/app/pkg/logging"
 	"github.com/sotah-inc/server/app/pkg/sotah"
 	"github.com/sotah-inc/server/app/pkg/state"
 	"github.com/sotah-inc/server/app/pkg/state/subjects"
+	"github.com/sotah-inc/server/app/pkg/store"
+	"google.golang.org/api/iterator"
 )
 
 var projectId = os.Getenv("GCP_PROJECT")
-var busClient bus.Client
-var regions sotah.RegionList
+
 var regionRealms map[blizzard.RegionName]sotah.Realms
+
+var busClient bus.Client
 var collectAuctionsTopic *pubsub.Topic
+
+var storeClient store.Client
+var auctionManifestStoreBase store.AuctionManifestBase
 
 func init() {
 	var err error
@@ -32,7 +37,6 @@ func init() {
 
 		return
 	}
-
 	collectAuctionsTopic, err = busClient.FirmTopic(string(subjects.CollectAuctionsCompute))
 	if err != nil {
 		log.Fatalf("Failed to get firm topic: %s", err.Error())
@@ -40,20 +44,34 @@ func init() {
 		return
 	}
 
-	msg, err := busClient.RequestFromTopic(string(subjects.Boot), "", 5*time.Second)
+	storeClient, err = store.NewClient(projectId)
 	if err != nil {
-		log.Fatalf("Failed to request boot data: %s", err.Error())
+		log.Fatalf("Failed to create new store client: %s", err.Error())
+
+		return
+	}
+	auctionManifestStoreBase = store.NewAuctionManifestBase(storeClient)
+
+	bootResponse, err := func() (state.AuthenticatedBootResponse, error) {
+		msg, err := busClient.RequestFromTopic(string(subjects.Boot), "", 5*time.Second)
+		if err != nil {
+			return state.AuthenticatedBootResponse{}, err
+		}
+
+		var out state.AuthenticatedBootResponse
+		if err := json.Unmarshal([]byte(msg.Data), &out); err != nil {
+			return state.AuthenticatedBootResponse{}, err
+		}
+
+		return out, nil
+	}()
+	if err != nil {
+		log.Fatalf("Failed to get authenticated-boot-response: %s", err.Error())
 
 		return
 	}
 
-	var bootResponse state.BootResponse
-	if err := json.Unmarshal([]byte(msg.Data), &bootResponse); err != nil {
-		log.Fatalf("Failed to unmarshal boot data: %s", err.Error())
-
-		return
-	}
-	regions = bootResponse.Regions
+	regions := bootResponse.Regions
 
 	regionRealms = map[blizzard.RegionName]sotah.Realms{}
 	for job := range busClient.LoadStatuses(regions) {
@@ -86,6 +104,30 @@ func AuctionsCollector(_ context.Context, m PubSubMessage) error {
 			}).Error("Failed to queue message")
 
 			continue
+		}
+	}
+
+	for _, realms := range regionRealms {
+		for _, realm := range realms {
+			bkt, err := auctionManifestStoreBase.ResolveBucket(realm)
+			if err != nil {
+				return err
+			}
+
+			it := bkt.Objects(storeClient.Context, nil)
+			objAttrs, err := it.Next()
+			if err != nil {
+				if err == iterator.Done {
+					break
+				}
+
+				return err
+			}
+
+			obj := bkt.Object(objAttrs.Name)
+			if err := obj.Delete(storeClient.Context); err != nil {
+				return err
+			}
 		}
 	}
 
