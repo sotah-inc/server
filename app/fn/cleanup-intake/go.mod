18cd057aefc04cec2f3a21734145b0fe127eb3db
Adrian Parker <ihsw.aparker@gmail.com>
2019-03-01 20:46:34 -0500

    Start filling in cleanup-intake func.

--- app/fn/cleanup-intake/go.mod
+++ app/fn/cleanup-intake/go.mod
@@ -1,7 +1,8 @@
 module github.com/sotah-inc/server/app/fn/cleanup-intake
 
 require (
-	cloud.google.com/go v0.36.0
+	cloud.google.com/go v0.36.0 // indirect
 	github.com/sirupsen/logrus v1.3.0
 	github.com/sotah-inc/server/app v0.0.0-20190302004137-4708d5a62184
+	google.golang.org/api v0.1.0
 )
--- app/fn/cleanup-intake/main.go
+++ app/fn/cleanup-intake/main.go
@@ -2,8 +2,20 @@ package cleanupintake
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+	"github.com/sotah-inc/server/app/pkg/logging"
+	"google.golang.org/api/iterator"
+
+	"github.com/sotah-inc/server/app/pkg/blizzard"
+	"github.com/sotah-inc/server/app/pkg/sotah"
+
+	"github.com/sotah-inc/server/app/pkg/bus"
 
 	"github.com/sotah-inc/server/app/pkg/store"
 )
@@ -30,5 +42,55 @@ type PubSubMessage struct {
 }
 
 func CleanupIntake(_ context.Context, m PubSubMessage) error {
+	job, err := func() (bus.CollectAuctionsJob, error) {
+		var in bus.Message
+		if err := json.Unmarshal(m.Data, &in); err != nil {
+			return bus.CollectAuctionsJob{}, err
+		}
+
+		var out bus.CollectAuctionsJob
+		if err := json.Unmarshal([]byte(in.Data), &out); err != nil {
+			return bus.CollectAuctionsJob{}, err
+		}
+
+		return out, nil
+	}()
+	if err != nil {
+		return err
+	}
+
+	region := sotah.Region{Name: blizzard.RegionName(job.RegionName)}
+	realm := sotah.Realm{
+		Realm:  blizzard.Realm{Slug: blizzard.RealmSlug(job.RealmSlug)},
+		Region: region,
+	}
+
+	bkt := auctionsStoreBase.GetBucket(realm)
+	it := bkt.Objects(storeClient.Context, nil)
+	for {
+		objAttrs, err := it.Next()
+		if err != nil {
+			if err == iterator.Done {
+				break
+			}
+
+			if err != nil {
+				return err
+			}
+		}
+
+		parts := strings.Split(objAttrs.Name, ".")
+		timestamp, err := strconv.Atoi(parts[0])
+		if err != nil {
+			return err
+		}
+
+		logging.WithFields(logrus.Fields{
+			"region":    region.Name,
+			"realm":     realm.Slug,
+			"timestamp": timestamp,
+		}).Info("Found")
+	}
+
 	return nil
 }
