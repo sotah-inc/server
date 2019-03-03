f8a71c5dcbd1b16a95543a6814c2cfb3c7d0b49d
Adrian Parker <ihsw.aparker@gmail.com>
2019-03-02 21:49:57 -0500

    Create deploy func.

---
+++ app/fn/auctions-cleanup-intake/deploy.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# us-central1 because us-east1 does not fucking have go111 runtime yet
+
+# deploying func
+gcloud functions deploy AuctionsCleanupIntake \
+    --runtime go111 \
+    --trigger-topic auctionsCleanupCompute \
+    --source . \
+    --memory 128MB \
+    --region us-central1
