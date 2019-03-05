fdd55ad6610e3cb3c37dcf6dbbafc8a46d33623e
Adrian Parker <ihsw.aparker@gmail.com>
2019-03-04 21:19:45 -0500

    Create func for transferring raw-auctions from v1 base to v2 base.

--- app/fn/cleanup-intake/main.go
+++ app/fn/cleanup-intake/main.go
@@ -3,17 +3,20 @@ package cleanupintake
 import (
 	"context"
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
 	"strings"
 	"time"
 
-	"github.com/sirupsen/logrus"
-	"github.com/sotah-inc/server/app/pkg/logging"
+	"cloud.google.com/go/storage"
+	"github.com/sotah-inc/server/app/pkg/util"
 
+	"github.com/sirupsen/logrus"
 	"github.com/sotah-inc/server/app/pkg/blizzard"
 	"github.com/sotah-inc/server/app/pkg/bus"
+	"github.com/sotah-inc/server/app/pkg/logging"
 	"github.com/sotah-inc/server/app/pkg/sotah"
 	"github.com/sotah-inc/server/app/pkg/store"
 	"google.golang.org/api/iterator"
@@ -24,6 +27,8 @@ var projectId = os.Getenv("GCP_PROJECT")
 var storeClient store.Client
 var auctionsStoreBase store.AuctionsBase
 var auctionManifestStoreBase store.AuctionManifestBase
+var auctionsStoreBaseV2 store.AuctionsBaseV2
+var newAuctionsBucket *storage.BucketHandle
 
 func init() {
 	var err error
@@ -36,38 +41,21 @@ func init() {
 	}
 	auctionsStoreBase = store.NewAuctionsBase(storeClient)
 	auctionManifestStoreBase = store.NewAuctionManifestBase(storeClient)
+	auctionsStoreBaseV2 = store.NewAuctionsBaseV2(storeClient)
+
+	newAuctionsBucket, err = auctionsStoreBaseV2.GetFirmBucket()
+	if err != nil {
+		log.Fatalf("Failed to get new raw-auctions bucket: %s", err.Error())
+
+		return
+	}
 }
 
 type PubSubMessage struct {
 	Data []byte `json:"data"`
 }
 
-func CleanupIntake(_ context.Context, m PubSubMessage) error {
-	job, err := func() (bus.CollectAuctionsJob, error) {
-		var in bus.Message
-		if err := json.Unmarshal(m.Data, &in); err != nil {
-			return bus.CollectAuctionsJob{}, err
-		}
-
-		var out bus.CollectAuctionsJob
-		if err := json.Unmarshal([]byte(in.Data), &out); err != nil {
-			return bus.CollectAuctionsJob{}, err
-		}
-
-		return out, nil
-	}()
-	if err != nil {
-		return err
-	}
-
-	logging.WithFields(logrus.Fields{"job": job}).Info("Handling")
-
-	region := sotah.Region{Name: blizzard.RegionName(job.RegionName)}
-	realm := sotah.Realm{
-		Realm:  blizzard.Realm{Slug: blizzard.RealmSlug(job.RealmSlug)},
-		Region: region,
-	}
-
+func RebuildManifest(realm sotah.Realm) error {
 	currentNormalizedTime := sotah.NormalizeTargetDate(time.Now())
 
 	manifests := map[sotah.UnixTimestamp]sotah.AuctionManifest{}
@@ -119,11 +107,177 @@ func CleanupIntake(_ context.Context, m PubSubMessage) error {
 		return err
 	}
 
-	for job := range auctionManifestStoreBase.WriteAll(manifestBucket, manifests) {
-		if job.Err != nil {
+	for writeAllOutJob := range auctionManifestStoreBase.WriteAll(manifestBucket, manifests) {
+		if writeAllOutJob.Err != nil {
 			return err
 		}
 	}
 
 	return nil
+}
+
+func TransferRawAuctions(realm sotah.Realm) error {
+	manifestBucket, err := auctionManifestStoreBase.GetFirmBucket(realm)
+	if err != nil {
+		return err
+	}
+
+	oldAuctionsBucket, err := auctionsStoreBase.GetFirmBucket(realm)
+	if err != nil {
+		return err
+	}
+
+	// spinning up the workers
+	in := make(chan *storage.ObjectAttrs)
+	out := make(chan error)
+	worker := func() {
+		for objAttrs := range in {
+			manifest, err := func() (sotah.AuctionManifest, error) {
+				obj := manifestBucket.Object(objAttrs.Name)
+				reader, err := obj.NewReader(storeClient.Context)
+				if err != nil {
+					return sotah.AuctionManifest{}, err
+				}
+				data, err := ioutil.ReadAll(reader)
+				if err != nil {
+					return sotah.AuctionManifest{}, err
+				}
+
+				var out sotah.AuctionManifest
+				if err := json.Unmarshal(data, &out); err != nil {
+					return sotah.AuctionManifest{}, err
+				}
+
+				return out, nil
+			}()
+			if err != nil {
+				out <- err
+
+				continue
+			}
+
+			for _, targetTimestamp := range manifest {
+				logging.WithFields(logrus.Fields{
+					"region":           realm.Region.Name,
+					"realm":            realm.Slug,
+					"target-timestamp": targetTimestamp,
+				}).Info("Transferring")
+
+				targetTime := time.Unix(int64(targetTimestamp), 0)
+				oldObj := auctionsStoreBase.GetObject(targetTime, oldAuctionsBucket)
+				exists, err := auctionsStoreBase.ObjectExists(oldObj)
+				if err != nil {
+					out <- err
+
+					continue
+				}
+
+				if !exists {
+					continue
+				}
+
+				reader, err := oldObj.NewReader(storeClient.Context)
+				if err != nil {
+					out <- err
+
+					continue
+				}
+
+				data, err := ioutil.ReadAll(reader)
+				if err != nil {
+					out <- err
+
+					continue
+				}
+
+				gzipEncoded, err := util.GzipEncode(data)
+				if err != nil {
+					out <- err
+
+					continue
+				}
+
+				newObj := auctionsStoreBaseV2.GetObject(realm, time.Unix(int64(targetTimestamp), 0), newAuctionsBucket)
+
+				// writing it out to the gcloud object
+				wc := newObj.NewWriter(storeClient.Context)
+				wc.ContentType = "application/json"
+				wc.ContentEncoding = "gzip"
+				if _, err := wc.Write(gzipEncoded); err != nil {
+					out <- err
+
+					continue
+				}
+				if err := wc.Close(); err != nil {
+					out <- err
+
+					continue
+				}
+
+				break
+			}
+		}
+	}
+	postWork := func() {
+		close(out)
+	}
+	util.Work(8, worker, postWork)
+
+	go func() {
+		it := manifestBucket.Objects(storeClient.Context, nil)
+		for {
+			objAttrs, err := it.Next()
+			if err != nil {
+				if err == iterator.Done {
+					break
+				}
+
+				if err != nil {
+					break
+				}
+			}
+
+			in <- objAttrs
+
+			break
+		}
+
+		close(in)
+	}()
+
+	for err := range out {
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func CleanupIntake(_ context.Context, m PubSubMessage) error {
+	job, err := func() (bus.CollectAuctionsJob, error) {
+		var in bus.Message
+		if err := json.Unmarshal(m.Data, &in); err != nil {
+			return bus.CollectAuctionsJob{}, err
+		}
+
+		var out bus.CollectAuctionsJob
+		if err := json.Unmarshal([]byte(in.Data), &out); err != nil {
+			return bus.CollectAuctionsJob{}, err
+		}
+
+		return out, nil
+	}()
+	if err != nil {
+		return err
+	}
+
+	logging.WithFields(logrus.Fields{"job": job}).Info("Handling")
+
+	realm := sotah.Realm{
+		Realm:  blizzard.Realm{Slug: blizzard.RealmSlug(job.RealmSlug)},
+		Region: sotah.Region{Name: blizzard.RegionName(job.RegionName)},
+	}
+
+	return TransferRawAuctions(realm)
 }
