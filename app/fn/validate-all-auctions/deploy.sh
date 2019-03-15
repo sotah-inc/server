9d5b062b4f0f0d1cb329605011b5b9a1654b497f
Adrian Parker <ihsw.aparker@gmail.com>
2019-03-15 10:49:59 -0400

    Fix typo.

--- app/fn/validate-all-auctions/deploy.sh
+++ app/fn/validate-all-auctions/deploy.sh
@@ -3,9 +3,9 @@
 # us-central1 because us-east1 does not fucking have go111 runtime yet
 
 # deploying func
-gcloud functions deploy ValidateAllLiveAuctions \
+gcloud functions deploy ValidateAllAuctions \
     --runtime go111 \
-    --trigger-topic validateAllLiveAuctions \
+    --trigger-topic validateAllAuctions \
     --source . \
     --memory 128MB \
     --timeout 500s \
