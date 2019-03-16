6395240119aa7e9e20dcb56cf0e8bfb42cf08aaf
Adrian Parker <ihsw.aparker@gmail.com>
2019-03-16 10:31:40 -0400

    Update download-all-auctions to fetch region-realms from the boot-base rather than boot topic.

--- app/fn/download-all-auctions/go.mod
+++ app/fn/download-all-auctions/go.mod
@@ -3,5 +3,5 @@ module github.com/sotah-inc/server/app/fn/download-all-auctions
 require (
 	cloud.google.com/go v0.36.0
 	github.com/sirupsen/logrus v1.3.0
-	github.com/sotah-inc/server/app v0.0.0-20190315144959-9d5b062b4f0f
+	github.com/sotah-inc/server/app v0.0.0-20190316142917-4ecbe90cc323
 )
--- app/fn/download-all-auctions/main.go
+++ app/fn/download-all-auctions/main.go
@@ -9,14 +9,15 @@ import (
 	"time"
 
 	"cloud.google.com/go/pubsub"
+	"cloud.google.com/go/storage"
 	"github.com/sirupsen/logrus"
 	"github.com/sotah-inc/server/app/pkg/blizzard"
 	"github.com/sotah-inc/server/app/pkg/bus"
 	"github.com/sotah-inc/server/app/pkg/bus/codes"
 	"github.com/sotah-inc/server/app/pkg/logging"
 	"github.com/sotah-inc/server/app/pkg/sotah"
-	"github.com/sotah-inc/server/app/pkg/state"
 	"github.com/sotah-inc/server/app/pkg/state/subjects"
+	"github.com/sotah-inc/server/app/pkg/store"
 )
 
 var (
@@ -50,36 +51,26 @@ func init() {
 		return
 	}
 
-	bootResponse, err := func() (state.AuthenticatedBootResponse, error) {
-		msg, err := busClient.RequestFromTopic(string(subjects.Boot), "", 5*time.Second)
-		if err != nil {
-			return state.AuthenticatedBootResponse{}, err
-		}
-
-		var out state.AuthenticatedBootResponse
-		if err := json.Unmarshal([]byte(msg.Data), &out); err != nil {
-			return state.AuthenticatedBootResponse{}, err
-		}
-
-		return out, nil
-	}()
+	storeClient, err := store.NewClient(projectId)
 	if err != nil {
-		log.Fatalf("Failed to get authenticated-boot-response: %s", err.Error())
+		log.Fatalf("Failed to create new store client: %s", err.Error())
 
 		return
 	}
 
-	regions := bootResponse.Regions
+	bootBase := store.NewBootBase(storeClient, "us-central1")
+	var bootBucket *storage.BucketHandle
+	bootBucket, err = bootBase.GetFirmBucket()
+	if err != nil {
+		log.Fatalf("Failed to get firm bucket: %s", err.Error())
 
-	regionRealms = map[blizzard.RegionName]sotah.Realms{}
-	for job := range busClient.LoadStatuses(regions) {
-		if job.Err != nil {
-			log.Fatalf("Failed to fetch status: %s", job.Err.Error())
+		return
+	}
+	regionRealms, err = bootBase.GetRegionRealms(bootBucket)
+	if err != nil {
+		log.Fatalf("Failed to get region-realms: %s", err.Error())
 
-			return
-		}
-
-		regionRealms[job.Region.Name] = job.Status.Realms
+		return
 	}
 }
 
