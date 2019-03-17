b4bee5664598725f8405bcabe677f6671f1b9ea5
Adrian Parker <ihsw.aparker@gmail.com>
2019-03-17 15:20:13 -0400

    Split listener into separate file.

--- app/pkg/state/state_prod_liveauctions.go
+++ app/pkg/state/state_prod_liveauctions.go
@@ -73,31 +73,3 @@ type ProdLiveAuctionsState struct {
 
 	LiveAuctionsBase store.LiveAuctionsBase
 }
-
-func (metricsState ProdLiveAuctionsState) ListenForComputedLiveAuctions(onReady chan interface{}, stop chan interface{}, onStopped chan interface{}) {
-	// establishing subscriber config
-	config := bus.SubscribeConfig{
-		Stop: stop,
-		Callback: func(busMsg bus.Message) {
-			tuples, err := bus.NewRegionRealmTimestampTuples(busMsg.Data)
-			if err != nil {
-				logging.WithField("error", err.Error()).Error("Failed to decode region-realm-timestamps tuples")
-
-				return
-			}
-
-			logging.WithField("tuples", len(tuples)).Info("Received tuples")
-
-			return
-		},
-		OnReady:   onReady,
-		OnStopped: onStopped,
-	}
-
-	// starting up worker for the subscription
-	go func() {
-		if err := metricsState.IO.BusClient.SubscribeToTopic(string(subjects.ReceiveComputedLiveAuctions), config); err != nil {
-			logging.WithField("error", err.Error()).Fatal("Failed to subscribe to topic")
-		}
-	}()
-}
---
+++ app/pkg/state/state_prod_liveauctions_receive.go
@@ -0,0 +1,35 @@
+package state
+
+import (
+	"github.com/sotah-inc/server/app/pkg/bus"
+	"github.com/sotah-inc/server/app/pkg/logging"
+	"github.com/sotah-inc/server/app/pkg/state/subjects"
+)
+
+func (metricsState ProdLiveAuctionsState) ListenForComputedLiveAuctions(onReady chan interface{}, stop chan interface{}, onStopped chan interface{}) {
+	// establishing subscriber config
+	config := bus.SubscribeConfig{
+		Stop: stop,
+		Callback: func(busMsg bus.Message) {
+			tuples, err := bus.NewRegionRealmTimestampTuples(busMsg.Data)
+			if err != nil {
+				logging.WithField("error", err.Error()).Error("Failed to decode region-realm-timestamps tuples")
+
+				return
+			}
+
+			logging.WithField("tuples", len(tuples)).Info("Received tuples")
+
+			return
+		},
+		OnReady:   onReady,
+		OnStopped: onStopped,
+	}
+
+	// starting up worker for the subscription
+	go func() {
+		if err := metricsState.IO.BusClient.SubscribeToTopic(string(subjects.ReceiveComputedLiveAuctions), config); err != nil {
+			logging.WithField("error", err.Error()).Fatal("Failed to subscribe to topic")
+		}
+	}()
+}
