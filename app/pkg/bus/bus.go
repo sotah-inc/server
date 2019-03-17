774f72da07a9514bb644417df540198596f050fe
Adrian Parker <ihsw.aparker@gmail.com>
2019-03-16 20:20:02 -0400

    Remove unnecessary logging.

--- app/pkg/bus/bus.go
+++ app/pkg/bus/bus.go
@@ -80,12 +80,6 @@ func (c Client) CreateTopic(id string) (*pubsub.Topic, error) {
 }
 
 func (c Client) Topic(topicName string) *pubsub.Topic {
-	if c.client == nil {
-		logging.Error("client was nil")
-	} else {
-		logging.Info("client was not nil")
-	}
-
 	return c.client.Topic(topicName)
 }
 
