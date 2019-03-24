aa27e184887c59d96caf60efb4006fcf12f37f9f
Adrian Parker <ihsw.aparker@gmail.com>
2019-03-24 15:08:36 -0400

    Fix instantiation of region-realm-timestamp-tuples from messages.

--- app/pkg/bus/bus.go
+++ app/pkg/bus/bus.go
@@ -604,12 +604,12 @@ type CollectAuctionsJob struct {
 func NewRegionRealmTimestampTuplesFromMessages(messages BulkRequestMessages) (RegionRealmTimestampTuples, error) {
 	tuples := RegionRealmTimestampTuples{}
 	for _, msg := range messages {
-		var respData RegionRealmTimestampTuple
-		if err := json.Unmarshal([]byte(msg.Data), &respData); err != nil {
+		tuple, err := NewRegionRealmTimestampTuple(msg.Data)
+		if err != nil {
 			return RegionRealmTimestampTuples{}, err
 		}
 
-		tuples = append(tuples, respData)
+		tuples = append(tuples, tuple)
 	}
 
 	return tuples, nil
@@ -710,6 +710,25 @@ func (s RegionRealmTimestampTuples) ToMessages() ([]Message, error) {
 	return out, nil
 }
 
+func NewRegionRealmTimestampTuple(data string) (RegionRealmTimestampTuple, error) {
+	base64Decoded, err := base64.StdEncoding.DecodeString(data)
+	if err != nil {
+		return RegionRealmTimestampTuple{}, err
+	}
+
+	gzipDecoded, err := util.GzipDecode(base64Decoded)
+	if err != nil {
+		return RegionRealmTimestampTuple{}, err
+	}
+
+	var out RegionRealmTimestampTuple
+	if err := json.Unmarshal(gzipDecoded, &out); err != nil {
+		return RegionRealmTimestampTuple{}, err
+	}
+
+	return out, nil
+}
+
 type RegionRealmTimestampTuple struct {
 	RegionName                string   `json:"region_name"`
 	RealmSlug                 string   `json:"realm_slug"`
@@ -720,12 +739,25 @@ type RegionRealmTimestampTuple struct {
 }
 
 func (t RegionRealmTimestampTuple) EncodeForDelivery() (string, error) {
-	data, err := json.Marshal(t)
+	jsonEncoded, err := json.Marshal(t)
 	if err != nil {
 		return "", err
 	}
 
-	return string(data), nil
+	gzipEncoded, err := util.GzipEncode(jsonEncoded)
+	if err != nil {
+		return "", err
+	}
+
+	return base64.StdEncoding.EncodeToString(gzipEncoded), nil
+}
+
+func (t RegionRealmTimestampTuple) Bare() RegionRealmTimestampTuple {
+	return RegionRealmTimestampTuple{
+		RegionName:      t.RegionName,
+		RealmSlug:       t.RealmSlug,
+		TargetTimestamp: t.TargetTimestamp,
+	}
 }
 
 type CleanupAuctionManifestJob = RegionRealmTimestampTuple
