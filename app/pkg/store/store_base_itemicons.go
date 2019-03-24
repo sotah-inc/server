46c24b276323230cbe7de3d51098487710031644
Adrian Parker <ihsw.aparker@gmail.com>
2019-03-23 20:07:06 -0400

    Rename to match convention.

--- app/pkg/store/store_base_itemicons.go
+++ app/pkg/store/store_base_itemicons.go
@@ -6,38 +6,38 @@ import (
 	"cloud.google.com/go/storage"
 )
 
-func NewItemIcons(c Client, location string) ItemIcons {
-	return ItemIcons{base{client: c, location: location}}
+func NewItemIconsBase(c Client, location string) ItemIconsBase {
+	return ItemIconsBase{base{client: c, location: location}}
 }
 
-type ItemIcons struct {
+type ItemIconsBase struct {
 	base
 }
 
-func (b ItemIcons) getBucketName() string {
+func (b ItemIconsBase) getBucketName() string {
 	return "item-icons"
 }
 
-func (b ItemIcons) GetBucket() *storage.BucketHandle {
+func (b ItemIconsBase) GetBucket() *storage.BucketHandle {
 	return b.base.getBucket(b.getBucketName())
 }
 
-func (b ItemIcons) GetFirmBucket() (*storage.BucketHandle, error) {
+func (b ItemIconsBase) GetFirmBucket() (*storage.BucketHandle, error) {
 	return b.base.getFirmBucket(b.getBucketName())
 }
 
-func (b ItemIcons) resolveBucket() (*storage.BucketHandle, error) {
+func (b ItemIconsBase) resolveBucket() (*storage.BucketHandle, error) {
 	return b.base.resolveBucket(b.getBucketName())
 }
 
-func (b ItemIcons) getObjectName(name string) string {
+func (b ItemIconsBase) getObjectName(name string) string {
 	return fmt.Sprintf("%s.jpg", name)
 }
 
-func (b ItemIcons) GetObject(name string, bkt *storage.BucketHandle) *storage.ObjectHandle {
+func (b ItemIconsBase) GetObject(name string, bkt *storage.BucketHandle) *storage.ObjectHandle {
 	return b.base.getObject(b.getObjectName(name), bkt)
 }
 
-func (b ItemIcons) GetFirmObject(name string, bkt *storage.BucketHandle) (*storage.ObjectHandle, error) {
+func (b ItemIconsBase) GetFirmObject(name string, bkt *storage.BucketHandle) (*storage.ObjectHandle, error) {
 	return b.base.getFirmObject(b.getObjectName(name), bkt)
 }
