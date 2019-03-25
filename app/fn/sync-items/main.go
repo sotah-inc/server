d153eb4f3c94b8d6de51b70d88e5efb2aaa865f2
Adrian Parker <ihsw.aparker@gmail.com>
2019-03-25 11:57:40 -0400

    Continue filling in sync-items functionality.

--- app/fn/sync-items/main.go
+++ app/fn/sync-items/main.go
@@ -6,17 +6,19 @@ import (
 	"log"
 	"os"
 
+	"cloud.google.com/go/pubsub"
 	"cloud.google.com/go/storage"
 	"github.com/sotah-inc/server/app/pkg/blizzard"
 	"github.com/sotah-inc/server/app/pkg/bus"
-	"github.com/sotah-inc/server/app/pkg/logging"
 	"github.com/sotah-inc/server/app/pkg/store"
+	"github.com/sotah-inc/server/app/pkg/util"
 )
 
 var (
 	projectId = os.Getenv("GCP_PROJECT")
 
-	busClient bus.Client
+	busClient               bus.Client
+	receiveSyncedItemsTopic *pubsub.Topic
 
 	blizzardClient blizzard.Client
 
@@ -60,6 +62,63 @@ func init() {
 	}
 }
 
+func SyncItem(id blizzard.ItemID) error {
+	return nil
+}
+
+type HandleJob struct {
+	Err error
+	Id  blizzard.ItemID
+}
+
+func Handle(ids blizzard.ItemIds) (blizzard.ItemIds, error) {
+	// spawning workers
+	in := make(chan blizzard.ItemID)
+	out := make(chan HandleJob)
+	worker := func() {
+		for id := range in {
+			if err := SyncItem(id); err != nil {
+				out <- HandleJob{
+					Err: err,
+					Id:  id,
+				}
+
+				continue
+			}
+
+			out <- HandleJob{
+				Err: nil,
+				Id:  id,
+			}
+		}
+	}
+	postWork := func() {
+		close(out)
+	}
+	util.Work(8, worker, postWork)
+
+	// spinning it up
+	go func() {
+		for _, id := range ids {
+			in <- id
+		}
+
+		close(in)
+	}()
+
+	// waiting for the results to drain out
+	results := blizzard.ItemIds{}
+	for outJob := range out {
+		if outJob.Err != nil {
+			return blizzard.ItemIds{}, outJob.Err
+		}
+
+		results = append(results, outJob.Id)
+	}
+
+	return results, nil
+}
+
 type PubSubMessage struct {
 	Data []byte `json:"data"`
 }
@@ -75,7 +134,21 @@ func SyncItems(_ context.Context, m PubSubMessage) error {
 		return err
 	}
 
-	logging.WithField("item-ids", len(itemIds)).Info("Processing batch")
+	results, err := Handle(itemIds)
+	if err != nil {
+		return err
+	}
+
+	data, err := results.EncodeForDelivery()
+	if err != nil {
+		return err
+	}
+
+	msg := bus.NewMessage()
+	msg.Data = data
+	if _, err := busClient.Publish(receiveSyncedItemsTopic, msg); err != nil {
+		return err
+	}
 
 	return nil
 }
