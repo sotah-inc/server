82b99721c2999be74c47c6a6a8fb14a0e08f6b8e
Adrian Parker <ihsw.aparker@gmail.com>
2019-04-08 20:34:16 -0400

    Fix typo.

--- app/fn/cleanup-pricelist-histories/go.mod
+++ app/fn/cleanup-pricelist-histories/go.mod
@@ -3,5 +3,5 @@ module github.com/sotah-inc/server/app/fn/cleanup-pricelist-histories
 require (
 	cloud.google.com/go v0.36.0
 	github.com/sirupsen/logrus v1.3.0
-	github.com/sotah-inc/server/app v0.0.0-20190409002657-43b2fa1a33ca
+	github.com/sotah-inc/server/app v0.0.0-20190409003307-6b9f9861e672
 )
--- app/fn/cleanup-pricelist-histories/main.go
+++ app/fn/cleanup-pricelist-histories/main.go
@@ -98,7 +98,7 @@ type PubSubMessage struct {
 	Data []byte `json:"data"`
 }
 
-func CleanupExpiredManifest(_ context.Context, m PubSubMessage) error {
+func CleanupPricelistHistories(_ context.Context, m PubSubMessage) error {
 	var in bus.Message
 	if err := json.Unmarshal(m.Data, &in); err != nil {
 		return err
