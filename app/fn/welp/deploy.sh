4c2e24ee077f847ec9867a31b0a41f85c8a499fc
Adrian Parker <ihsw.aparker@gmail.com>
2019-04-09 20:04:29 -0400

    Start testing vpc-connector functionality.

--- app/fn/welp/deploy.sh
+++ app/fn/welp/deploy.sh
@@ -3,8 +3,10 @@
 # us-central1 because us-east1 does not fucking have go111 runtime yet
 
 # deploying func
-gcloud functions deploy Welp \
+gcloud beta functions deploy Welp \
     --runtime go111 \
+    --vpc-connector projects/$PROJECT_ID/locations/us-central1/connectors/sotah-connector \
+    --env-vars-file .env.yaml \
     --trigger-topic welp \
     --source . \
     --memory 512MB \
--- app/fn/welp/main.go
+++ app/fn/welp/main.go
@@ -6,44 +6,27 @@ import (
 	"os"
 	"strconv"
 
-	"github.com/sirupsen/logrus"
-
 	"cloud.google.com/go/storage"
-	"github.com/sotah-inc/server/app/pkg/blizzard"
-	"github.com/sotah-inc/server/app/pkg/bus"
 	"github.com/sotah-inc/server/app/pkg/logging"
+	"github.com/sotah-inc/server/app/pkg/messenger"
+	"github.com/sotah-inc/server/app/pkg/state/subjects"
 	"github.com/sotah-inc/server/app/pkg/store"
-	"github.com/sotah-inc/server/app/pkg/util"
-	"google.golang.org/api/iterator"
 )
 
 var (
 	projectId = os.Getenv("GCP_PROJECT")
+	natsHost  = os.Getenv("NATS_HOST")
+	natsPort  = os.Getenv("NATS_PORT")
 
 	storeClient store.Client
 	bootBase    store.BootBase
 	bootBucket  *storage.BucketHandle
-	itemsBase   store.ItemsBase
-	itemsBucket *storage.BucketHandle
 
-	busClient bus.Client
+	messengerClient messenger.Messenger
 )
 
 func init() {
 	var err error
-	busClient, err = bus.NewClient(projectId, "fn-welp")
-	if err != nil {
-		log.Fatalf("Failed to create new bus client: %s", err.Error())
-
-		return
-	}
-
-	storeClient, err = store.NewClient(projectId)
-	if err != nil {
-		log.Fatalf("Failed to create new store client: %s", err.Error())
-
-		return
-	}
 
 	bootBase = store.NewBootBase(storeClient, "us-central1")
 	bootBucket, err = bootBase.GetFirmBucket()
@@ -53,44 +36,19 @@ func init() {
 		return
 	}
 
-	itemsBase = store.NewItemsBase(storeClient, "us-central1")
-	itemsBucket, err = itemsBase.GetFirmBucket()
+	var parsedNatsPort int
+	parsedNatsPort, err = strconv.Atoi(natsPort)
 	if err != nil {
-		log.Fatalf("Failed to get firm bucket: %s", err.Error())
+		log.Fatalf("Failed to parse nats port: %s", err.Error())
 
 		return
 	}
-}
 
-type ValidateJob struct {
-	Err           error
-	Id            blizzard.ItemID
-	HasObjectName bool
-	HasObjectURI  bool
-}
-
-func Validate(id blizzard.ItemID) ValidateJob {
-	obj, err := itemsBase.GetFirmObject(id, itemsBucket)
+	messengerClient, err = messenger.NewMessenger(natsHost, parsedNatsPort)
 	if err != nil {
-		return ValidateJob{
-			Err: err,
-			Id:  id,
-		}
-	}
+		log.Fatalf("Failed to get messenger client: %s", err.Error())
 
-	item, err := itemsBase.NewItem(obj)
-	if err != nil {
-		return ValidateJob{
-			Err: err,
-			Id:  id,
-		}
-	}
-
-	return ValidateJob{
-		Err:           nil,
-		Id:            id,
-		HasObjectName: item.IconObjectName != "",
-		HasObjectURI:  item.IconURL != "",
+		return
 	}
 }
 
@@ -109,74 +67,12 @@ func Welp(_ context.Context, _ PubSubMessage) error {
 		return nil
 	}
 
-	// spinning up workers
-	logging.Info("Spinning up workers")
-	in := make(chan blizzard.ItemID)
-	out := make(chan ValidateJob)
-	worker := func() {
-		for id := range in {
-			out <- Validate(id)
-		}
-	}
-	postWork := func() {
-		close(out)
-	}
-	util.Work(32, worker, postWork)
-
-	// enqueueing it up
-	logging.Info("Queueing it up")
-	go func() {
-		it := itemsBucket.Objects(storeClient.Context, nil)
-		for {
-			objAttrs, err := it.Next()
-			if err != nil {
-				if err == iterator.Done {
-					break
-				}
-
-				logging.WithField("error", err.Error()).Error("Failed to iterate to next")
-
-				break
-			}
-
-			parsed, err := strconv.Atoi(objAttrs.Name[0:(len(objAttrs.Name) - len(".json.gz"))])
-			if err != nil {
-				logging.WithField("error", err.Error()).Error("Failed to parse name")
-
-				break
-			}
-
-			in <- blizzard.ItemID(parsed)
-		}
-
-		close(in)
-	}()
-
-	// waiting for results to drain out
-	logging.Info("Waiting for results to drain out")
-	total := 0
-	missing := 0
-	for job := range out {
-		if job.Err != nil {
-			return err
-		}
-
-		total += 1
-
-		if !job.HasObjectURI || !job.HasObjectName {
-			logging.WithFields(logrus.Fields{
-				"id":              job.Id,
-				"has-object-uri":  job.HasObjectURI,
-				"has-object-name": job.HasObjectName,
-			}).Info("Does not have both object-uri and object-name")
-			missing += 1
-		}
+	resp, err := messengerClient.Request(string(subjects.Boot), []byte{})
+	if err != nil {
+		return err
 	}
 
-	logging.WithFields(logrus.Fields{
-		"total":   total,
-		"missing": missing,
-	}).Info("Done!")
+	logging.WithField("response", len(resp.Data)).Info("Received response")
 
 	return nil
 }
