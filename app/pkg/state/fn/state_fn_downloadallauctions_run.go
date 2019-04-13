dc7b401859784de66dfef9cb86f0d7cf5e0aaa8a
Adrian Parker <ihsw.aparker@gmail.com>
2019-04-12 20:54:54 -0400

    Fix log line.

--- app/pkg/state/fn/state_fn_downloadallauctions_run.go
+++ app/pkg/state/fn/state_fn_downloadallauctions_run.go
@@ -118,7 +118,7 @@ func (sta DownloadAllAuctionsState) PublishToReceiveRealms(tuples bus.RegionReal
 		return err
 	}
 
-	logging.WithField("realms", jsonEncoded).Info("Sending realms")
+	logging.WithField("realms", string(jsonEncoded)).Info("Sending realms")
 
 	req, err := sta.IO.Messenger.Request(string(subjects.ReceiveRealms), jsonEncoded)
 	if err != nil {
