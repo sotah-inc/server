147b864bca12e2bb7487a379b894e646e9d40058
Adrian Parker <ihsw.aparker@gmail.com>
2019-05-06 17:30:16 -0400

    Start base hell client.

--- app/pkg/hell/hell.go
+++ app/pkg/hell/hell.go
@@ -1,1 +1,27 @@
 package hell
+
+import (
+	"context"
+
+	"cloud.google.com/go/firestore"
+)
+
+func NewClient(projectId string) (Client, error) {
+	ctx := context.Background()
+	firestoreClient, err := firestore.NewClient(ctx, projectId)
+	if err != nil {
+		return Client{}, err
+	}
+
+	return Client{
+		Context:   ctx,
+		client:    firestoreClient,
+		projectID: projectId,
+	}, nil
+}
+
+type Client struct {
+	Context   context.Context
+	projectID string
+	client    *firestore.Client
+}
