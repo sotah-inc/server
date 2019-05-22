992dd686d18cd11f9950fd30f0bea30e990798c1
Adrian Parker <ihsw.aparker@gmail.com>
2019-05-22 19:23:39 -0400

    Update prod-api listener to return appropriate data.

--- app/pkg/state/state_prod_api_realmmodificationdates.go
+++ app/pkg/state/state_prod_api_realmmodificationdates.go
@@ -2,11 +2,8 @@ package state
 
 import (
 	nats "github.com/nats-io/go-nats"
-	"github.com/sotah-inc/server/app/pkg/blizzard"
-	"github.com/sotah-inc/server/app/pkg/logging"
 	"github.com/sotah-inc/server/app/pkg/messenger"
 	mCodes "github.com/sotah-inc/server/app/pkg/messenger/codes"
-	"github.com/sotah-inc/server/app/pkg/sotah"
 	"github.com/sotah-inc/server/app/pkg/state/subjects"
 )
 
@@ -14,44 +11,7 @@ func (sta ProdApiState) ListenForRealmModificationDates(stop ListenStopChan) err
 	err := sta.IO.Messenger.Subscribe(string(subjects.RealmModificationDates), stop, func(natsMsg nats.Msg) {
 		m := messenger.NewMessage()
 
-		req, err := NewRealmModificationDatesRequest(natsMsg.Data)
-		if err != nil {
-			m.Err = err.Error()
-			m.Code = mCodes.GenericError
-			sta.IO.Messenger.ReplyTo(natsMsg, m)
-
-			return
-		}
-
-		logging.WithField("hell-region-realms", sta.HellRegionRealms.Total()).Info("Checking hell-region-realms")
-
-		hellRealms, ok := sta.HellRegionRealms[blizzard.RegionName(req.RegionName)]
-		if !ok {
-			m.Err = "region not found"
-			m.Code = mCodes.NotFound
-			sta.IO.Messenger.ReplyTo(natsMsg, m)
-
-			return
-		}
-
-		hellRealm, ok := hellRealms[blizzard.RealmSlug(req.RealmSlug)]
-		if !ok {
-			m.Err = "realm not found"
-			m.Code = mCodes.NotFound
-			sta.IO.Messenger.ReplyTo(natsMsg, m)
-
-			return
-		}
-
-		res := RealmModificationDatesResponse{
-			RealmModificationDates: sotah.RealmModificationDates{
-				Downloaded:                 int64(hellRealm.Downloaded),
-				LiveAuctionsReceived:       int64(hellRealm.LiveAuctionsReceived),
-				PricelistHistoriesReceived: int64(hellRealm.PricelistHistoriesReceived),
-			},
-		}
-
-		encodedData, err := res.EncodeForDelivery()
+		encodedData, err := sta.HellRegionRealms.ToRegionRealmModificationDates().EncodeForDelivery()
 		if err != nil {
 			m.Err = err.Error()
 			m.Code = mCodes.GenericError
